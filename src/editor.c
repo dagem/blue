6a2ad4083b99701ffbadbe1dc52e26a9b988d0a2
dagem <dagem46@hotmail.com>
2023-01-22 16:34:08 -0500

    finished search

--- src/editor.c
+++ src/editor.c
@@ -288,11 +288,12 @@ void editorDelChar()
 	{
 		return;
 	}
+	
 	erow *row = &E.row[E.cy];
 	if(E.cx > 0)
 	{
-	editorRowDelChar(row, E.cx-1);
-	E.cx--;
+		editorRowDelChar(row, E.cx-1);
+		E.cx--;
 	}
 	else
 	{
@@ -522,7 +523,7 @@ void editorFind()
 	int saved_cy = E.cy;
 	int saved_sx = E.rowoff;
 	int saved_sy = E.coloff;
-	char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+	char *query = editorPrompt("Search: %s ESC to cancel, ← or ↑ to search backwards ↓ or → for forwards", editorFindCallback);
 	if(query)
 	{
 		free(query);
@@ -537,18 +538,51 @@ void editorFind()
 }
 void editorFindCallback(char *query, int key)
 {
+	static int last_match = -1;
+	static int direction = 1;
+
 	if(key == '\r' || key == '\x1b')
 	{
+		last_match = -1;
+		direction = 1;
 		return;
 	}
+	else if(key == ARROW_RIGHT || key == ARROW_DOWN)
+	{
+		direction = 1;
+	}
+	else if(key == ARROW_LEFT || key == ARROW_UP)
+	{
+		direction = -1;
+	}
+	else
+	{
+		direction = 1;
+		last_match = -1;
+	}
+	if(last_match == -1)
+	{
+		direction = 1;
+	}
+	int current = last_match;
 	int i;
 	for(i = 0; i < E.numrows; i++)
 	{
-		erow *row = &E.row[i];
+		current += direction;
+		if(current == -1)
+		{
+			current = E.numrows-1;
+		}
+		else if(current == E.numrows)
+		{
+			current = 0;
+		}
+		erow *row = &E.row[current];
 		char *match = strstr(row->render, query);
 		if(match)
 		{
-			E.cy = i;
+			last_match = current;
+			E.cy = current;
 			E.cx = editorRowRxToCx(row, match - row->render);
 			E.rowoff = E.numrows;
 			break;
@@ -608,6 +642,7 @@ void editorProcessKeypress()
 		case BACKSPACE:
 		case CTRL_KEY('h'):
 		case DEL_KEY:
+
 			if(c == DEL_KEY)
 			{
 				editorMoveCursor(ARROW_RIGHT);
